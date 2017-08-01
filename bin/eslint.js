a57669678d97f03b8a3835d3d9225ae46bebc81e
Peter Velkov <kidroca@gmail.com>
2017-08-01 16:00:24 +0300

    Update bin/eslint.js to use lib/cli.js

--- bin/eslint.js
+++ bin/eslint.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 'use strict';
 
-require('./eslint_d');
+const useStdIn = (process.argv.indexOf("--stdin") > -1);
+
+const concat = require("concat-stream"),
+  cli = require("../lib/cli"),
+  path = require("path"),
+  fs = require("fs");
+
+process.once("uncaughtException", err => {
+
+  console.error("\nOops! Something went wrong! :(");
+  console.error(err.stack);
+  process.exitCode = 1;
+});
+
+if (useStdIn) {
+  process.stdin.pipe(concat({ encoding: "string" }, text => {
+    process.exitCode = cli.execute(process.argv, text);
+  }));
+}
+else {
+  process.exitCode = cli.execute(process.argv);
+}
